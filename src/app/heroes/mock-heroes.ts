b92e84eec1aab6386710f775b209ec9943edfb5b
LyubovDevialtovskaya <luba1297mail@gmail.com>
2021-08-20 14:10:29 +0300

    изменила стиль, проверка

--- src/app/heroes/mock-heroes.ts
+++ src/app/heroes/mock-heroes.ts
@@ -1,4 +1,5 @@
-import { Hero } from './hero';
+import { Hero } from "./hero";
+
 
 export const HEROES: Hero[] = [
   { id: 11, name: 'Dr Nice' },
@@ -11,4 +12,4 @@ export const HEROES: Hero[] = [
   { id: 18, name: 'Dr IQ' },
   { id: 19, name: 'Magma' },
   { id: 20, name: 'Tornado' }
-];
+]
--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MessageService {
-messages: string[]=[];
-  constructor() { }
-  add (message: string){
+  messages: string[] = [];
+
+  add(message: string) {
     this.messages.push(message);
   }
-clear (){
-  this.messages =[];
-}
+
+  clear() {
+    this.messages = [];
+  }
 }
